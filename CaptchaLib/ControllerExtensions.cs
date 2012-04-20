44eced5ee51de513108064a482ed24422bc2d860
Leonid Gordo <leonardpt@gmail.com>
2012-04-20 12:13:34 +0400

    add more features to controller extensions

--- CaptchaLib/ControllerExtensions.cs
+++ CaptchaLib/ControllerExtensions.cs
@@ -17,8 +17,18 @@ namespace CaptchaLib
 {
     public static class ControllerExtensions
     {
+        internal const string CaptchaUniqueIdPrefix = "_CAPTCHA_";
+
+        public static CaptchaResult Captcha(this Controller controller)
+        {
+            return Captcha(controller, new CaptchaImage());
+        }
+
         public static CaptchaResult Captcha(this Controller controller, ICaptchaImage captchaImage, int quality = 50, int width = 150, int height = 45 )
         {
+            var captchaValue = captchaImage as ICaptchaValue;
+            if (captchaValue != null)
+                controller.Session[CaptchaUniqueIdPrefix + captchaImage.CaptchaUniqueId] = captchaValue.RenderedValue;
             return new CaptchaResult(captchaImage, quality, width, height);
         }
     }
--- CaptchaLib/ICaptchaImage.cs
+++ CaptchaLib/ICaptchaImage.cs
@@ -19,5 +19,12 @@ namespace CaptchaLib
     {
         void SaveImageToStream(Stream outputStream, int quality, int width, int height);
         void SaveImageToStream(Stream outputStream, int quality, int width, int height, string s);
+        string CaptchaUniqueId { get; set; }
+    }
+
+    internal interface ICaptchaValue
+    {
+        string RenderedValue { get; }
     }
 }
+
