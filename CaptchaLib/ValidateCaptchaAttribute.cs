bf5e9ccc9b9da9d8e78a848ed3121e491455d49c
Leonid Gordo <leonardpt@gmail.com>
2012-05-24 13:54:34 +0400

    fix files headers

--- CaptchaLib/ValidateCaptchaAttribute.cs
+++ CaptchaLib/ValidateCaptchaAttribute.cs
@@ -1,5 +1,5 @@
 ﻿/*
-ASP.NET MVC Web Application Captcha Library Copyright (C) 2009-2012 Leonid Leonid Gordo
+ASP.NET MVC Web Application Captcha Library Copyright (C) 2009-2012 Leonid Gordo
 
 This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation; 
 either version 3.0 of the License, or (at your option) any later version.
