a90439bd5ca4d6e363940a1c94afac0432450bde
Leonid Gordo <leonardpt@gmail.com>
2016-02-20 15:48:56 +0300

    fix for console-less browsers

--- js/captcha.js
+++ js/captcha.js
@@ -3,7 +3,9 @@
         var cmd = $(this);
         var url = cmd.prev(".captchaContainer").find("img").first().attr("src");
         url = url.replace(/(.*?)\?r=\d+\.\d+$/, "$1")
-        console.log(url);
+        if (typeof console != "undefined") {
+			console.log(url);
+		}
         var img = new Image();
         $(img).load(function () {
             cmd.prev(".captchaContainer").empty().append(img);
